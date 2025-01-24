939309af8832327cbe4f7a25de95893555366cba
Lucas Carozza <lucas.carozza@icloud.com>
2025-01-24 13:27:59 -0300

    Update Subreddits list

--- src/components/Subreddit/Subreddit.jsx
+++ src/components/Subreddit/Subreddit.jsx
@@ -1,11 +1,16 @@
 import { FaReddit } from "react-icons/fa";
 import styles from "./Subreddit.module.css";
 
+const subreddit = {
+  name: "r/EvilCats",
+  img: "./public/evil-smile.jpg",
+}
+
 const Subreddit = () => {
   return (
-    <div className={styles.subreddits}>
-      <FaReddit />
-      <p>Subreddit</p>
+    <div className={styles.subreddit}>
+      <img src={subreddit.img} className={styles.subredditImg} />
+      <p className={styles.subredditName}>{subreddit.name}</p>
     </div>
   );
 };
