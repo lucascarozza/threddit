479b20323eb70074dcbcf779e662a9a6454cce3b
Lucas Carozza <lucas.carozza@icloud.com>
2025-01-24 16:22:55 -0300

    Rework Post and add link support

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./Header.module.css";
 import { FaRedditAlien } from "react-icons/fa6";
+// import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 const Header = () => {
+  const isLight = true;
+
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>
+      <div className={styles.headerLeft}>
         <FaRedditAlien className={styles.icon} />
         <h1 className={styles.logo}>threddit</h1>
       </div>
-      <SearchBar />
+      <div className={styles.headerRight}>
+        <SearchBar />
+        {/* {isLight ? <MdLightMode className={styles.themeIcon} /> : <MdDarkMode className={styles.themeIcon} />} */}
+      </div>
     </header>
   );
 };
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,15 +1,21 @@
+import { FaLink } from "react-icons/fa";
 import styles from "./Post.module.css";
 
 const subreddit = {
   name: "r/EvilCats",
   time: "30 min ago",
   img: "/evil-smile.jpg",
-  postText: "evil cat smiling",
+  postTitle: "Evil cat smiling",
+  postText:
+    "Whiskers McSneaky's Evil Cat Smile has the whole neighborhood on edge—who knows what mischievous plot he's cooking up this time? That gleam in his eye and that up-to-no-good grin can only mean one thing: Whiskers is back at it again! #EvilCatSmile #MischiefMonday #CatPranks 😼",
+  link: "https://en.wikipedia.org/wiki/Cat",
   media: "/evil-smile.jpg",
 };
 
 const Post = () => {
   const hasMedia = true;
+  const hasText = true;
+  const hasLink = true;
 
   return (
     <div className={styles.card}>
@@ -18,8 +24,20 @@ const Post = () => {
         <p className={styles.subredditName}>r/EvilCats • {subreddit.time}</p>
       </div>
       <div className={styles.textArea}>
-        <p className={styles.postText}>{subreddit.postText}</p>
+        <p className={styles.postTitle}>{subreddit.postTitle}</p>
+        {hasText ? <p className={styles.postText}>{subreddit.postText}</p> : ""}
       </div>
+
+      {hasLink ? (
+        <div className={styles.linkArea}>
+          <a href={subreddit.link} target="_blank" className={styles.link}>
+            <FaLink /> {subreddit.link}
+          </a>
+        </div>
+      ) : (
+        ""
+      )}
+
       {hasMedia ? (
         <div className={styles.mediaArea}>
           <figure>
