e2d2b172f8e7cf7d05d286629d9592cd10a46810
Lucas Carozza <lucas.carozza@icloud.com>
2025-01-30 01:39:04 -0300

    Add search functionality

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,40 @@
 import { FaSearch } from "react-icons/fa";
 import styles from "./SearchBar.module.css";
+import { useDispatch } from "react-redux";
+import { searchPosts } from "../../features/feedSlice";
+import { useState, useEffect } from "react";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSearch = () => {
+    const sanitizedTerm = searchTerm.replace(/[^\w\s.,-]/gi, "").trim();
+    dispatch(searchPosts(sanitizedTerm));
+  };
+
+  useEffect(() => {
+    if (searchTerm) {
+      handleSearch();
+    }
+  }, [searchTerm]);
+
   return (
-    <form className={styles.searchForm}>
+    <form
+      className={styles.searchForm}
+      name="Search"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
       <input
         type="search"
         placeholder="search"
         className={styles.searchInput}
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
       ></input>
       <button className={styles.searchButton}>
         <FaSearch className={styles.searchIcon} />
